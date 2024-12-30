31e829d9ce44ec4aeb632bff8a5ea26c90be94e7
awurorawr <174339922+awurorawr@users.noreply.github.com>
2024-12-30 20:03:17 +0900

    Another commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import playformCompress from '@playform/compress';
+
+import compressor from 'astro-compressor';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), playformCompress(), compressor(), icon()],
 });
